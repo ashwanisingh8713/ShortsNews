76301d2426fbcebb0fefa5dca638422ddaa5eef4
Kmlesh Patel <pkamlesh0124@gmail.com>
2023-04-25 17:40:06 +0530

    resolved android core in all module.

--- shortsnews/src/main/java/com/ns/shortsnews/MainApplication.kt
+++ shortsnews/src/main/java/com/ns/shortsnews/MainApplication.kt
@@ -1,16 +1,11 @@
 package com.ns.shortsnews
 
 import android.app.Application
-import org.koin.core.context.startKoin
 
 class MainApplication:Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin {
-            startKoin{
 
-            }
         }
 
-    }
 }
