bf80b19d6d96de40d4ee2e3b9a0e13e1657f7d8c
Kmlesh Patel <pkamlesh0124@gmail.com>
2023-04-26 10:57:05 +0530

    Koin dependency

--- shortsnews/src/main/java/com/ns/shortsnews/MainApplication.kt
+++ shortsnews/src/main/java/com/ns/shortsnews/MainApplication.kt
@@ -1,10 +1,18 @@
 package com.ns.shortsnews
 
 import android.app.Application
+import org.koin.core.KoinApplication
+import org.koin.core.context.startKoin
 
 class MainApplication:Application() {
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            // declare used Android context
+          KoinApplication.init()
+            // declare modules
+            modules()
+        }
 
         }
 
